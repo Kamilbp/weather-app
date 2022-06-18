ff1c0eed61110e8472bf0c2feb7b83ba5977da28
Kamil Biegajło <myxirecs@gmail.com>
2022-06-18 18:23:38 +0200

    fix code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./Components/Forms/Form";
 import Weather from "./Components/Weather/Weather";
 
 function App() {
-  console.log("app running");
 
   const [running, setRunning] = useState(false);
   const [coordinates, setCoordinates] = useState({
-    lat: null,
-    lon: null
+    lat: "",
+    lon: "",
   });
   const getLocationHandler = async (cityName) => {
     console.log(cityName);
     setRunning(true);
     setCoordinates({
-      lat: null,
-      lon: null,
+      ...coordinates,
+      lat: "",
+      lon: "",
     });
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=3569019c79759949d934004dd6afd4c9`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=3569019c79759949d934004dd6afd4c9`
     );
     const data = await response.json();
-    console.log(data[0].lat);
-    let updatedCoordinates = {
+    console.log(response.status);
+    console.log(data[0].lat)
+
+    setCoordinates({
+      ...coordinates,
       lat: data[0].lat,
       lon: data[0].lon,
-    }
-
-    setCoordinates(() => ({...updatedCoordinates}));
-    console.log(coordinates);
+    });
   };
+
   return (
     <>
       <div className="main-container">
         <Form onEnteredCityName={getLocationHandler} />
-        {running && <Weather coordinates={`${coordinates.lat} ${coordinates.lon}`}/>}
+        {/* {running && <Weather latCord={coordinates.lat} lonCord={coordinates.lon}/>} */}
       </div>
     </>
   );
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import Cart from "../UI/Cart";
 
-import classes from './Weather.module.css';
+import classes from "./Weather.module.css";
 
 const Weather = (props) => {
-  return <Cart>
-    <div className={classes.weather}>
-      <p>{props.coordinates}</p>
-    </div>
-  </Cart>;
+  const callCurrWeather = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${props.latCord}&lon=${props.lonCord}&appid=3569019c79759949d934004dd6afd4c9`
+    );
+
+    const data = await response.json();
+    console.log(data)
+  };
+  
+  return (
+    <Cart>
+      <div className={classes.weather}>
+        <p>tutaj bedzie pogoda</p>
+      </div>
+    </Cart>
+  );
 };
 
 export default Weather;
