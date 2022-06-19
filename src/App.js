b099614fd47f93606dcb57c8f03cf7ab9c5a3d39
Kamil Biegajło <myxirecs@gmail.com>
2022-06-19 14:36:03 +0200

    Adding weather icon and desc

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Form from "./Components/Forms/Form";
 import Weather from "./Components/Weather/Weather";
 
-function App() {
 
+function App() {
   const [running, setRunning] = useState(false);
-  const [coordinates, setCoordinates] = useState({
-    lat: "",
-    lon: "",
-  });
+  const [weather, setWeather] = useState("");
+  const [iconId, setIconId] = useState("");
+
+  let content;
+
   const getLocationHandler = async (cityName) => {
     console.log(cityName);
     setRunning(true);
-    setCoordinates({
-      ...coordinates,
-      lat: "",
-      lon: "",
-    });
-    const response = await fetch(
+    const geocodeResponse = await fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=3569019c79759949d934004dd6afd4c9`
     );
-    const data = await response.json();
-    console.log(response.status);
-    console.log(data[0].lat)
+    const geocodeData = await geocodeResponse.json();
+    // console.log(response.status);
+    // console.log(data[0].lat)
+    const updatedCoords = {
+      lat: geocodeData[0].lat,
+      lon: geocodeData[0].lon,
+    };
+    const weatherResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${updatedCoords.lat}&lon=${updatedCoords.lon}&appid=3569019c79759949d934004dd6afd4c9`
+    );
 
-    setCoordinates({
-      ...coordinates,
-      lat: data[0].lat,
-      lon: data[0].lon,
-    });
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+    setWeather(weatherData.weather[0].main);
+    setIconId(weatherData.weather[0].icon);
   };
+
+  if (running === true){
+    const weatherSvg = (
+      <img
+        src={`http://openweathermap.org/img/wn/${iconId}@2x.png`}
+        alt="weather icon"
+      />
+    );
+    content = <Weather weatherIcon={weatherSvg} weatherName={weather}/>;
+  }
+
 
   return (
     <>
       <div className="main-container">
         <Form onEnteredCityName={getLocationHandler} />
-        {/* {running && <Weather latCord={coordinates.lat} lonCord={coordinates.lon}/>} */}
+        {running && content}
       </div>
     </>
   );
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,22 +1,15 @@
-import { useEffect } from "react";
+
 import Cart from "../UI/Cart";
 
 import classes from "./Weather.module.css";
 
 const Weather = (props) => {
-  const callCurrWeather = async () => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${props.latCord}&lon=${props.lonCord}&appid=3569019c79759949d934004dd6afd4c9`
-    );
 
-    const data = await response.json();
-    console.log(data)
-  };
-  
   return (
     <Cart>
       <div className={classes.weather}>
-        <p>tutaj bedzie pogoda</p>
+        <p>{props.weatherIcon}</p>
+        <p>{props.weatherName}</p>
       </div>
     </Cart>
   );
